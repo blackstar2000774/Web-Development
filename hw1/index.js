539cddf3b25f79127043f11a4b76de68dcfa3032
blackstar2000774 <samip2397@gmail.com>
2025-01-17 18:47:05 +0545

    hw

---
+++ hw1/index.js
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <Title>Hello World in JavaScript</Title>
+    </head>
+    <body>
+        <script type+"text/JavaScript">
+        document.write("Hello World");
+        </script>
+    </body>
+</html>
