63937869a920633fe56db8860614dadf5f56e4e8
blackstar2000774 <samip2397@gmail.com>
2025-02-10 07:21:58 +0545

    Create App.js

---
+++ Portfolio/todo-app/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  
+  const addTodo = (text) => {
+    const newTodo = {
+      text,
+      isCompleted: false,
+      id: Date.now(),
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      )
+    );
+  };
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div className="app">
+      <h1>Todo List</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} toggleComplete={toggleComplete} removeTodo={removeTodo} />
+    </div>
+  );
+}
+
+export default App;
